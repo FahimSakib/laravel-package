ce14c783c44f873ae58152b4b725f89b3c550e43
FahimSakib <fsakib991@gmail.com>
2022-03-30 00:07:11 +0600

    adding/meging custom js file in a separate custom file in public by mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+    .scripts(['resources/asset/js/custom/custom.js'],'public/js/custom.js')
+    .sourceMaps()
